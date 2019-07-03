c1989605aab85663ba300e1801b99a0494b43d6c
Paul Müller <dev@craban.de>
2019-07-03 09:56:17 +0200

    build: add link to applications in dmg container

--- .travis/osx_build_app.sh
+++ .travis/osx_build_app.sh
@@ -32,8 +32,15 @@ pip install pyinstaller
 # otherwise PyPI deployment on travis-CI tries to upload *.dmg files.
 pyinstaller -w -y --distpath="./dist_app" --additional-hooks-dir=".travis" $SCRIPT
 
+# add link to Applications
+mkdir ui-release
+cd ui-release
+ln -s /Applications
+cd ..
+mv ${APP} ui-release/
+
 # create temporary DMG
-hdiutil create -srcfolder "${APP}" -volname "${NAMEVERSION}" -fs HFS+ \
+hdiutil create -srcfolder ui-release/ -volname "${NAMEVERSION}" -fs HFS+ \
         -fsargs "-c c=64,a=16,e=16" -format UDRW "${TMP}"
 
 # optional: edit the DMG

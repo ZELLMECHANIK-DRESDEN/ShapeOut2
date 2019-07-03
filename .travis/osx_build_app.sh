57591873d62ca045f7cfe16bb9491cc3e65e3964
Paul Müller <dev@craban.de>
2019-07-03 08:43:08 +0200

    ci: update build recipe from https://github.com/paulmueller/travisCI-macOSx-PyQt-pyinstaller

--- .travis/osx_build_app.sh
+++ .travis/osx_build_app.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+# Builds a macOS app in a DMG container using PyInstaller and an app launcher.
+# usage:
+#     osx_build_app.sh AppName [AppVersion]
+#
+# Notes:
+# - AppVersion is optional (only used for name of DMG container)
+# - This script must be called from the root directory of the repository
+# - The file ./travis/AppNameApp.py [sic] must be present (relative
+#   to root of the repository)
 
 if [ -z $1 ]; then
     echo "Please specify package name as command line argument!"
     exit 1
 fi
-
 NAME=$1
+
+if [ -z $2 ]; then
+    NAMEVERSION=${1}
+else
+    NAMEVERSION=${1}_${2}
+fi
+
 # append "App" to avoid naming conflicts with python library
-SCRIPT=".travis/${1}App.py"
-APP="./dist_app/${1}App.app"
-DMG="./dist_app/${1}.dmg"
+SCRIPT=".travis/${NAME}App.py"
+APP="./dist_app/${NAME}App.app"
+DMG="./dist_app/${NAMEVERSION}.dmg"
 TMP="./dist_app/pack.temp.dmg"
 pip install pyinstaller
 
@@ -18,7 +33,7 @@ pip install pyinstaller
 pyinstaller -w -y --distpath="./dist_app" --additional-hooks-dir=".travis" $SCRIPT
 
 # create temporary DMG
-hdiutil create -srcfolder "${APP}" -volname "${NAME}" -fs HFS+ \
+hdiutil create -srcfolder "${APP}" -volname "${NAMEVERSION}" -fs HFS+ \
         -fsargs "-c c=64,a=16,e=16" -format UDRW "${TMP}"
 
 # optional: edit the DMG

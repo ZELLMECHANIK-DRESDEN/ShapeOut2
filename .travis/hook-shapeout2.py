4f1959831d0b613c787038985dd6bc96a1802748
Paul Müller <dev@craban.de>
2019-08-01 09:42:20 +0200

    setup: bumpy dclab version

--- .travis/hook-shapeout2.py
+++ .travis/hook-shapeout2.py
@@ -7,7 +7,7 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-# Hook for PyJibe: https://github.com/AFM-analysis/PyJibe
+# Hook for PyJibe: https://github.com/ZellMechanik-Dresden/ShapeOut2
 from PyInstaller.utils.hooks import collect_data_files
 
 # Data files

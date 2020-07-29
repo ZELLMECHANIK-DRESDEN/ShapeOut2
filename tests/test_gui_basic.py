74e9c7b71056ed400e0db2c450998408d6a0d4e3
Paul Müller <dev@craban.de>
2020-07-29 13:52:50 +0200

    set system excepthook to None for testing

--- tests/test_gui_basic.py
+++ tests/test_gui_basic.py
@@ -1,5 +1,6 @@
 """Test of data set functionalities"""
 import pathlib
+import sys
 
 from PyQt5 import QtCore
 
@@ -12,6 +13,7 @@ import pytest
 def run_around_tests():
     # Code that will run before your test, for example:
     session.clear_session()
+    sys.excepthook = None
     # A test function will be run at this point
     yield
     # Code that will run after your test, for example:

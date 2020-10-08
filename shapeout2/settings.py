a0b6db029b6447a4f1dc26203beb88c8885414b8
Paul Müller <dev@craban.de>
2020-10-08 10:56:46 +0200

    setup: bump dclab to 0.29.0 and fix windows installation directory

--- shapeout2/settings.py
+++ shapeout2/settings.py
@@ -4,8 +4,10 @@ import pathlib
 
 import appdirs
 
-#: default settings file name
-NAME = "shapeout2.cfg"
+#: application name
+NAME = "Shape-Out 2"
+#: application author
+ORG = "Zellmechanik-Dresden"
 
 #: default configuration parameters
 DEFAULTS = {
@@ -23,13 +25,14 @@ DEFAULTS = {
 class SettingsFile(object):
     """Manages a settings file in the user's config dir"""
 
-    def __init__(self, name=NAME, defaults=DEFAULTS, directory=None):
+    def __init__(self, name=NAME, org=ORG, defaults=DEFAULTS, directory=None):
         """Initialize settings file (create if it does not exist)"""
         if directory is None:
-            directory = appdirs.user_config_dir()
-        fname = pathlib.Path(directory) / name
+            directory = appdirs.user_config_dir(appname=name, appauthor=org)
+        fname = pathlib.Path(directory) / (name.replace(" ", "_") + ".cfg")
         # create file if not existent
         if not fname.exists():
+            fname.parent.mkdir(exist_ok=True, parents=True)
             # Create the file
             fname.open("w").close()
 

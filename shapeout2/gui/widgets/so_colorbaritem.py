4dd9ba99152f5c81a5f90fcf2c07f28ca074bf65
Paul Müller <dev@craban.de>
2021-11-30 17:24:43 +0100

    fix: mysterious behavior of subclassed pg.ColorBarItem (close #109)

--- shapeout2/gui/widgets/so_colorbaritem.py
+++ shapeout2/gui/widgets/so_colorbaritem.py
@@ -2,7 +2,7 @@ import pyqtgraph as pg
 
 
 class ShapeOutColorBarItem(pg.ColorBarItem):
-    def __init__(self, yoffset, height, label, *args, **kwargs):
+    def __init__(self, yoffset, height, label, colorMap, **kwargs):
         """pg.ColorBarItem modified for Shape-Out
 
         - Added option to define height
@@ -10,7 +10,15 @@ class ShapeOutColorBarItem(pg.ColorBarItem):
         - show the label on the right-hand axis
         - increase the contents margins
         """
-        super(ShapeOutColorBarItem, self).__init__(*args, **kwargs)
+        super(ShapeOutColorBarItem, self).__init__(
+            colorMap=colorMap,
+            # TODO: Removing `cmap=colorMap` results in grayscale colormaps.
+            # https://github.com/ZELLMECHANIK-DRESDEN/ShapeOut2/issues/109
+            # This does not make sense at all. But maybe some mysteries are
+            # meant to stay hidden indefinitely. Maybe in July 2022, when
+            # pyqtgraph drops the "cmap" argument, it will just work...
+            cmap=colorMap,
+            **kwargs)
 
         # show label on right side
         self.axis.setLabel(label)

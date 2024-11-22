c54952e7d479ebd7134827cd7c967de0a27a8c65
Paul Müller <dev@craban.de>
2024-11-22 15:52:31 +0100

    ref: remove distutils dependency

--- tests/test_gui_update.py
+++ tests/test_gui_update.py
@@ -1,5 +1,3 @@
-import encodings.idna  # noqa: F401
-import os
 import socket
 
 import pytest
@@ -15,10 +13,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         NET_AVAILABLE = False
 
 
-@pytest.mark.xfail(os.getenv("APPVEYOR") in ["true", "True"],
-                   reason="does not always run on Appveyor")
-@pytest.mark.xfail(os.getenv("CI") == "true",
-                   reason="does not always run on travisCI")
 @pytest.mark.skipif(not NET_AVAILABLE, reason="No network connection!")
 def test_update_basic():
     mdict = update.check_release(ghrepo="ZELLMECHANIK-DRESDEN/ShapeOut2",

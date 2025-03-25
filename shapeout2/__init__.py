884b91c7b35f6dd1d0f2b7418cb2507e35432fd5
Paul Müller <dev@craban.de>
2025-03-26 00:06:49 +0100

    docs: properly employ license GPL-3.0-or-later

--- shapeout2/__init__.py
+++ shapeout2/__init__.py
@@ -1,1 +1,17 @@
+"""GUI for analysis and visualization of deformability cytometry datasets
+Copyright (C) 2019 Paul Müller
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 from ._version import version as __version__  # noqa: F401
